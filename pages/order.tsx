bd9bb90b0fdb45b75b47e54f488bc6247307eb37
kirito desu <53758449+c98022414@users.noreply.github.com>
2022-03-14 16:23:11 +0800

    order

--- pages/order.tsx
+++ pages/order.tsx
@@ -12,6 +12,7 @@ import Layout from '@/components/layout'
 import PageHeader from '@/components/pageHeader/cover'
 import { Empty, Pagination, Row } from 'antd'
 import Spinner from '@/components/spinner'
+import { CloseOutlined } from '@ant-design/icons'
 
 const MyOrder = () => {
   const router = useRouter()
@@ -79,11 +80,32 @@ const MyOrder = () => {
       <PageHeader title={`My Order`} image={`/cover5.jpeg`} height={400} />
       <StyledMyOrder>
         <div className="container">
+          <h1>Order list</h1>
           {orderData ? (
             orderData.count ? (
-              <div>
+              <StyledList>
                 {orderData.rows.map((item) => {
-                  return <div key={item.id}>{item.code}</div>
+                  return (
+                    <div className="order-item" key={item.id}>
+                      <div className="image">
+                        <img src="" alt="" srcSet="" />
+                      </div>
+                      <div className="item-info">
+                        <div className="title">
+                          <div>Chew Golden </div>
+                          <div className="extra">
+                            <CloseOutlined />
+                          </div>
+                        </div>
+                        <div className="bottom">
+                          <div className="total">212</div>
+                          <div className="amount">
+                            <span>3,000</span> $
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
                 })}
                 {orderData?.count > limit ? (
                   <Row justify="end">
@@ -95,7 +117,7 @@ const MyOrder = () => {
                     />
                   </Row>
                 ) : null}
-              </div>
+              </StyledList>
             ) : (
               <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
             )
@@ -108,8 +130,71 @@ const MyOrder = () => {
   )
 }
 
+const StyledList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  .order-item {
+    padding: 20px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    display: flex;
+    min-width: 100%;
+
+    .bottom {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      flex-direction: row;
+
+      .total {
+        font-size: 1.2em;
+      }
+      .amount {
+        font-size: 1.2em;
+        span {
+          color: var(--primary-red);
+          font-weight: 700;
+        }
+      }
+    }
+    .title {
+      display: flex;
+      justify-content: space-between;
+      min-width: 100%;
+      font-size: 1.2em;
+      margin-bottom: 15px;
+      align-items: center;
+      .extra {
+        font-size: 1em;
+        cursor: pointer;
+        :hover {
+          color: var(--primary);
+        }
+      }
+    }
+    .item-info {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+    }
+    .image {
+      margin-right: 20px;
+      img {
+        height: 70px;
+        width: 70px;
+        object-fit: cover;
+      }
+    }
+  }
+`
+
 const StyledMyOrder = styled.div`
-  height: 500px;
+  min-height: 500px;
+
+  h1 {
+    font-weight: 700;
+    color: var(--primary);
+  }
 `
 
 export default MyOrder
