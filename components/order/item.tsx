ff7b61516a8e0bcfbd1b16ac4a812c07078b1513
Bilegsaikhan <bilegsaikhan.e@gmail.com>
2022-03-15 13:16:21 +0800

    qweqwe

--- components/order/item.tsx
+++ components/order/item.tsx
@@ -13,64 +13,74 @@ const OrderItem = ({
   mutate: any
 }): JSX.Element => {
   return (
-    <StyledWrapper>
-      <div className="item-info">
-        <div className="if-top">
-          <div className="item-name">
-            <h2>
+    <Link href={`/order/${item.id}`} passHref>
+      <StyledWrapper>
+        <div className="item-info">
+          <div className="if-top">
+            <div className="item-name">
+              <h2>
+                <Space>
+                  <span>Order no:</span>
+                  <span>{item.code}</span>
+                </Space>
+              </h2>
+            </div>
+            <div>
               <Space>
-                <span>Order no:</span>
-                <Link href={`/order/${item.id}`}>
-                  <a>{item.code}</a>
-                </Link>
+                <span>Order date:</span>
+                <span>{dateFormat(item.created_at)}</span>
               </Space>
-            </h2>
+            </div>
           </div>
-          <div>
-            <Space>
-              <span>Order date:</span>
-              <span>{dateFormat(item.created_at)}</span>
-            </Space>
-          </div>
-        </div>
-        <div className="if-bottom">
-          <div className="ifb-quantity">
-            <Space>
-              <span>Order status:</span>
-              <Tag>
-                {item.order_status} -{' '}
-                <span>{dateFormat(item.order_status_date)}</span>
-              </Tag>
-            </Space>
-          </div>
-          <div className="ifb-price">
-            <Space>
-              <h2>Paid amount:</h2>
-              <h2>${item.paid_amount}</h2>
-            </Space>
+          <div className="if-bottom">
+            <div className="ifb-quantity">
+              <Space>
+                <span>Order status:</span>
+                <Tag>
+                  {item.order_status} -{' '}
+                  <span>{dateFormat(item.order_status_date)}</span>
+                </Tag>
+              </Space>
+            </div>
+            <div className="ifb-price">
+              <Space>
+                <h2>Paid amount:</h2>
+                <h2>${item.paid_amount}</h2>
+              </Space>
+            </div>
           </div>
         </div>
-      </div>
-    </StyledWrapper>
+      </StyledWrapper>
+    </Link>
   )
 }
 
-const StyledWrapper = styled.div`
+const StyledWrapper = styled.a`
   border-radius: 4px;
   padding: 1rem;
   display: flex;
   border: 1px solid rgba(0, 0, 0, 0.1);
   margin-bottom: 2rem;
+  color: #333;
+
+  &:hover {
+    color: #333;
+    border: 1px solid var(--primary);
+  }
+
   .item-info {
     flex: 1;
     display: flex;
     flex-direction: column;
+    h2 {
+      margin-bottom: 0;
+    }
     .if-top {
       display: flex;
       align-items: flex-start;
+      justify-content: space-between;
       margin-bottom: 15px;
       .item-name {
-        flex: 1;
         margin-right: 20px;
         & a {
           color: #221906;
@@ -83,6 +93,34 @@ const StyledWrapper = styled.div`
       display: flex;
       justify-content: space-between;
       align-items: center;
+    }
+
+    @media (max-width: 1024px) {
+      .if-top {
+        margin-bottom: 0;
+      }
+      .if-top,
+      .if-bottom {
+        flex-wrap: wrap;
+        .ifb-price {
+          display: flex;
+          justify-content: flex-end;
+          width: 100%;
+          border-top: 1px solid #eee;
+          margin-top: 1rem;
+          padding-top: 1rem;
+        }
+        & > div {
+          width: 100%;
+          &.item-name {
+            margin-right: 0;
+          }
+          .ant-space {
+            width: 100%;
+            justify-content: space-between;
+          }
+        }
+      }
     }
   }
 `
