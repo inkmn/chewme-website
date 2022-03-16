e2f86c7946acf60514bc65c821cc2d2995629c98
kirito desu <53758449+c98022414@users.noreply.github.com>
2022-03-16 12:50:13 +0800

    fix

--- pages/order/[order_id].tsx
+++ pages/order/[order_id].tsx
@@ -66,136 +66,147 @@ const OrderDetail = () => {
       <StyledOrderDetail>
         <div className="container">
           <h1>Order no: {orderData?.code}</h1>
-          <Row gutter={[16, 16]}>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-              <TotalStyled>
-                <div className="head">
-                  <div className="headCol">
-                    <h3>Shipping addres</h3>
-                    <div className="shipping-box">
-                      <Space>
-                        <span>{orderData?.order_shipping?.last_name}</span>
-                        <span>{orderData?.order_shipping?.first_name}</span>
-                      </Space>
-                      <span>
-                        {orderData?.order_shipping?.country_code || '1010'}
-                        <span className="desc">(country code)</span>
-                      </span>
-                      <span>
-                        {orderData?.order_shipping?.state_code}
-                        <span className="desc">(state)</span>
-                      </span>
-                      <span>
-                        {orderData?.order_shipping?.city_code}
-                        <span className="desc">(city code)</span>
-                      </span>
-                      <span>{orderData?.order_shipping?.phone}</span>
-                      <span>{orderData?.order_shipping?.address}</span>
-                    </div>
-                  </div>
-
-                  <div className="headCol">
-                    <h3>Order info</h3>
-                    <div className="shipping-box">
-                      <div className="orderStatus">
-                        <Space>
-                          <span>{orderData?.code}</span>
-                          {renderStatus(orderData?.order_status)}
-                        </Space>
-                      </div>
-                      <span>{datetimeFormat(orderData?.created_at)}</span>
-                    </div>
-                  </div>
-
-                  <div className="headCol">
-                    <h3 className="text-end">Total</h3>
-
-                    <div className="shipping-box paidAmount text-end">
-                      {formatterUSD(orderData?.total_amount)}
-                    </div>
-                  </div>
+          <TotalStyled>
+            <div className="head">
+              <div className="headCol">
+                <h3>Shipping addres</h3>
+                <div className="shipping-box">
+                  <Space>
+                    <span>{orderData?.order_shipping?.last_name}</span>
+                    <span>{orderData?.order_shipping?.first_name}</span>
+                  </Space>
+                  <span>
+                    {orderData?.order_shipping?.country_code || '1010'}
+                    <span className="desc">(country code)</span>
+                  </span>
+                  <span>
+                    {orderData?.order_shipping?.state_code}
+                    <span className="desc">(state)</span>
+                  </span>
+                  <span>
+                    {orderData?.order_shipping?.city_code}
+                    <span className="desc">(city code)</span>
+                  </span>
+                  <span>{orderData?.order_shipping?.phone}</span>
+                  <span>{orderData?.order_shipping?.address}</span>
                 </div>
-              </TotalStyled>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
-              <ProductsTable>
-                <tr className="header">
-                  <td>Description</td>
-                  <td>Unit Cost</td>
-                  <td>Quantity</td>
-                  <td>Amount</td>
-                </tr>
+              </div>
 
-                {orderData?.products?.map((item, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>
-                        <div className="title">{item?.name}</div>
-                        <p className="description">{item.code}</p>
-                      </td>
-                      <td>{formatterUSD(item?.price)}</td>
-                      <td>{item.quantity}</td>
-                      <td>{formatterUSD(item?.total_amount)}</td>
-                    </tr>
-                  )
-                })}
-                <tr style={{ border: 'none' }}>
-                  <td></td>
-                  <td></td>
-                  <td>Total Quantity</td>
-                  <td>{orderData?.quantity}</td>
-                </tr>
-                <tr style={{ border: 'none' }}>
-                  <td></td>
-                  <td></td>
-                  <td>shipping additional fee</td>
-                  <td className="color-red bold">
-                    {formatterUSD(orderData?.shipping_additional_fee)}
-                  </td>
-                </tr>
-              </ProductsTable>
-            </Col>
-          </Row>
-          <pre>{JSON.stringify(orderData, null, 2)}</pre>
+              <div className="headCol">
+                <h3>Order info</h3>
+                <div className="shipping-box">
+                  <div className="orderStatus">
+                    <Space>
+                      <span>{orderData?.code}</span>
+                      {renderStatus(orderData?.order_status)}
+                    </Space>
+                  </div>
+                  <span>{datetimeFormat(orderData?.created_at)}</span>
+                </div>
+              </div>
+
+              <div className="headCol">
+                <h3 className="text-end">Total</h3>
+
+                <div className="shipping-box paidAmount text-end">
+                  {formatterUSD(orderData?.total_amount)}
+                </div>
+              </div>
+            </div>
+          </TotalStyled>
+          {/* <pre>{JSON.stringify(orderData, null, 2)}</pre> */}
+
+          <ProductsTable>
+            <table>
+              <tr className="list-header">
+                <td>Description</td>
+                <td>Unit Cost</td>
+                <td>Quantity</td>
+                <td>Amount</td>
+              </tr>
+
+              {orderData?.products?.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <div className="title">{item?.name}</div>
+                      <p className="description">{item.code}</p>
+                    </td>
+                    <td>{formatterUSD(item?.price)}</td>
+                    <td>{item.quantity}</td>
+                    <td>{formatterUSD(item?.total_amount)}</td>
+                  </tr>
+                )
+              })}
+              <tr style={{ border: 'none' }}>
+                <td></td>
+                <td></td>
+                <td>Total Quantity</td>
+                <td>{orderData?.quantity}</td>
+              </tr>
+              <tr style={{ border: 'none' }}>
+                <td></td>
+                <td></td>
+                <td>shipping additional fee</td>
+                <td className="bold color-primary">
+                  {formatterUSD(orderData?.shipping_additional_fee)}
+                </td>
+              </tr>
+            </table>
+          </ProductsTable>
         </div>
       </StyledOrderDetail>
     </Layout>
   )
 }
 
-const ProductsTable = styled.table`
+const ProductsTable = styled.div`
   width: 100%;
-  border-top: 2px solid var(--primary);
-  color: #333;
+  overflow: auto;
+  padding-bottom: 10px;
+  margin-top: 10px;
 
-  .color-red {
-    color: #e8213a;
-  }
-  .bold {
-    font-weight: 600;
-  }
+  table {
+    width: 100%;
+    border-top: 2px solid var(--primary);
+    color: #333;
 
-  tr {
-    border-bottom: 1px solid #dadada;
-  }
-  td {
-    font-size: 1.2rem;
-    padding: 15px 0;
-    width: max-content;
-    min-width: 100px;
-    p {
-      margin: 0;
-      color: #8e8e8e;
-      font-size: 1rem;
-      width: 300px;
+    .color-primary {
+      color: var(--primary);
     }
-    text-align: end;
-    :first-child {
-      text-align: start;
+    .color-red {
+      color: #e8213a;
     }
-  }
-  .header {
-    color: var(--primary);
+    .bold {
+      font-weight: 600;
+    }
+
+    tr {
+      border-bottom: 1px solid #dadada;
+    }
+    td {
+      font-size: 1.2rem;
+      padding: 15px 0;
+      width: max-content;
+      min-width: 100px;
+      p {
+        margin: 0;
+        font-size: 1rem;
+        width: 300px;
+        span {
+          color: #8e8e8e;
+          margin-left: 8px;
+          font-size: 1rem;
+        }
+      }
+      text-align: end;
+      :first-child {
+        text-align: start;
+      }
+    }
+    .list-header {
+      color: var(--primary);
+    }
   }
 `
 
@@ -232,10 +243,17 @@ const TotalStyled = styled.div`
       font-size: 2rem;
     }
   }
-
   .head {
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 10px;
+  }
+
+  @media only screen and (max-width: 790px) {
+    .headCol {
+      min-width: 100%;
+    }
   }
 `
 
