5b1cdf7ae1fb2779658198d0daf1fd3f65593df7
Bilegsaikhan <bilegsaikhan.e@gmail.com>
2022-03-17 19:30:40 +0800

    qweqweq

---
+++ hooks/useProduct.ts
@@ -0,0 +1,89 @@
+import useSWR from 'swr'
+import privatefetcher from '@/lib/privateFetch'
+import CartType from '@/interfaces/cart'
+import { useState } from 'react'
+import qs from 'qs'
+
+const useCart = (): {
+  data: {
+    rows: CartType[]
+    count: number
+    filter_bars: 
+  }
+  error: {
+    status: number
+    data: any
+  }
+  isValidating: boolean
+  mutate: () => void
+  handlePageChange: (page: number) => void
+  offset: {
+    page: number
+    limit: number
+  }
+} => {
+  const apiUrl = '/app/order/carts'
+  const [offset, setOffset] = useState({
+    page: 1,
+    limit: 12,
+  })
+  const queryObj = {
+    offset,
+    filter: {
+      query: '',
+      category_id: '',
+      start_date: '',
+      end_date: '',
+    },
+  }
+  const [queryString, setQueryString] = useState(
+    qs.stringify(queryObj, {
+      encode: false,
+      addQueryPrefix: true,
+    })
+  )
+
+  const handlePageChange = (offset: any) => {
+    setOffset(offset)
+    setQueryString(
+      qs.stringify(
+        {
+          ...queryObj,
+          offset,
+        },
+        {
+          encode: false,
+          addQueryPrefix: true,
+        }
+      )
+    )
+  }
+
+  const {
+    data: cartData = [],
+    error,
+    isValidating,
+    mutate,
+  } = useSWR<CartType[]>(`${apiUrl}${queryString}`, privatefetcher)
+
+  return {
+    data: {
+      rows: cartData,
+      count: cartData.length,
+      cart_sum: {
+        total_amount: cartData.reduce(
+          (acc, cur) => acc + cur.price * cur.quantity,
+          0
+        ),
+        total_count: cartData.reduce((acc, cur) => acc + cur.quantity, 0),
+      },
+    },
+    error,
+    isValidating,
+    mutate,
+    handlePageChange,
+    offset,
+  }
+}
+
+export default useCart
