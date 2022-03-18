874b315ef1782788f98ab6413422996aa65c6478
kirito desu <53758449+c98022414@users.noreply.github.com>
2022-03-18 09:36:44 +0800

    fix

--- pages/settings/[settings_id].tsx
+++ pages/settings/[settings_id].tsx
@@ -6,10 +6,8 @@ import ShippingAddress from '@/components/forms/shippingAddress'
 import UserPassword from '@/components/forms/userPassword'
 import UserEmail from '@/components/forms/userEmail'
 import classnames from 'classnames'
-import { CloseOutlined } from '@ant-design/icons'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import useUser from '@/hooks/useUser'
 
 const { TabPane } = Tabs
 const Settings = () => {
@@ -76,21 +74,6 @@ const Settings = () => {
             </Col>
             <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}></Col>
           </Row>
-
-          {/* <Row gutter={[24, 24]}>
-            <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
-              <h1>Shipping Address</h1>
-              <ShippingAddress />
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
-              <h1>Password changes</h1>
-              <UserPassword />
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={12} xl={12} xxl={12}>
-              <h1>Email changes</h1>
-              <UserEmail />
-            </Col>
-          </Row> */}
           {/* This is Settings view
           <pre>{JSON.stringify(user, null, 2)}</pre> */}
         </div>
